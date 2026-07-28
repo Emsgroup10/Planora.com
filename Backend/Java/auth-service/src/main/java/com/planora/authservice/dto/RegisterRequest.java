b13dbfb0194ccd104a8f932dde656fe464657df3
ankita <ankitakhatavkar2003@gmail.com>
2026-07-28 16:44:07 +0530

    Restructuring project

---
+++ Backend/Java/auth-service/src/main/java/com/planora/authservice/dto/RegisterRequest.java
@@ -0,0 +1,53 @@
+package com.planora.authservice.dto;
+
+public class RegisterRequest {
+
+    private Integer rid;
+    private String email;
+    private String password;
+    private Long phoneNo;
+    private String address;
+
+    public RegisterRequest() {
+    }
+
+    public Integer getRid() {
+        return rid;
+    }
+
+    public void setRid(Integer rid) {
+        this.rid = rid;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Long getPhoneNo() {
+        return phoneNo;
+    }
+
+    public void setPhoneNo(Long phoneNo) {
+        this.phoneNo = phoneNo;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
---
+++ Backend/Java/auth-service/src/main/java/com/planora/authservice/entity/Role.java
@@ -0,0 +1,10 @@
+package com.planora.authservice.entity;
+
+public enum Role {
+
+    ADMIN,
+    ORGANIZER,
+    VENDOR,
+    CUSTOMER
+
+}
